cf276a5133dfeab9960fb2d59f02c732ab5c3165
wdsjk <mamaevivan543@gmail.com>
2024-03-13 22:48:30 +0300

    damn my bad

--- src/main/java/wdsjk/project/timetrackerassignment/repository/UserRepository.java
+++ src/main/java/wdsjk/project/timetrackerassignment/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package wdsjk.project.timetrackerassignment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import wdsjk.project.timetrackerassignment.domain.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-//    @Query(value = "select * from usr where username='#{#username}'", nativeQuery = true)
     Optional<User> findUserByUsername(String username);
 }
